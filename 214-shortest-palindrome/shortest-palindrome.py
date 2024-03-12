54fce072b6cf8a50cd3a8f40fd7c9ad118a08baf
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-03-12 11:33:10 -0400

    Time: 38 ms (99.91%) | Memory: 16.7 MB (95.17%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,16 +1,26 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
+        # Base case: If the input string is empty, return an empty string
         if not s:
             return ''
         
+        # Initialize index `i` to track the length of the palindrome suffix
         i = 0
-        for r in range(len(s) -1, -1 , -1):
+        
+        # Iterate over the string `s` from the end towards the beginning
+        for r in range(len(s) - 1, -1, -1):
+            # Check if characters at index `i` and `r` are equal
             if s[i] == s[r]:
                 i += 1
         
+        # If `i` has traversed the entire string, it's already a palindrome
         if i == len(s):
             return s
         
+        # Extract the non-palindromic suffix
         suffix = s[i:]
+        # Reverse the suffix to get the prefix
         prefix = suffix[::-1]
+        # Recursively construct the shortest palindrome by adding reversed prefix,
+        # the shortest palindrome of the remaining prefix, and the original suffix
         return prefix + self.shortestPalindrome(s[:i]) + suffix
