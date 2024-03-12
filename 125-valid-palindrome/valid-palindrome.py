89ad466f8986d99d1813f98870798cfc9583537e
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-03-12 09:21:22 -0400

    Time: 44 ms (62.64%) | Memory: 17.3 MB (49.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,14 +1,24 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # Check if the input string is empty or contains only whitespace, if so, it's considered a palindrome
         if s == ' ':
             return True
+        
+        # Remove non-alphanumeric characters from the string and convert it to lowercase
         s = ''.join(letter for letter in s if letter.isalnum())
         s = s.lower()
-        print(s)
+        
+        # Initialize two pointers, one pointing to the start of the string and the other to the end
         left, right = 0, len(s) - 1
+        
+        # Iterate through the string from both ends towards the middle
         while left < right:
+            # If characters at current positions don't match, the string is not a palindrome
             if s[left] != s[right]:
                 return False
+            # Move the pointers towards the middle
             left += 1
             right -= 1
+        
+        # If the loop completes without finding any mismatch, the string is a palindrome
         return True
