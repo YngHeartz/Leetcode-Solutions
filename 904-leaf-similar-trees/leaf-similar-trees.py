260b5bf939eb1d8be191b3b89c5a6b1de24abbbd
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-05-22 16:30:21 -0400

    Time: 33 ms (76.51%) | Memory: 16.5 MB (94.08%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -8,24 +8,18 @@ class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         
         def dfs(root, leaf):
-            #Base Case
             if not root:
                 return
             
-            #If leaf is found append val to leaf
             if not root.right and not root.left:
                 leaf.append(root.val)
+                return
             
-            #dfs function for the right and left getting all leafs appended to the leaf array
             dfs(root.right, leaf)
             dfs(root.left, leaf)
 
-        #Creates empty arrays that will store the appened values
         leaf1, leaf2 = [], []
-
-        #Recursive call for the root1 and root2
         dfs(root1, leaf1)
         dfs(root2, leaf2)
 
-        #Comparison between leaf1 array and leaf2 array
         return leaf1 == leaf2
