47b5a4e6f6f603eec4ab4030a2b6bace9bd76734
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-05-21 21:04:44 -0400

    Time: 31 ms (84.10%) | Memory: 16.6 MB (22.34%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.py
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.py
@@ -6,12 +6,18 @@
 #         self.right = right
 class Solution:
     def checkTree(self, root: Optional[TreeNode]) -> bool:
+        #Create empty list
         res = []
 
+        #Helper function
         def findval(root):
+            #If not root return
             if not root:
                 return
+            #Get right and left values and store them into res
             res.append(root.left.val)
             res.append(root.right.val)
+        #Call the function on the root
         findval(root)
+        #Return the sum of res and check if its equal to the root.val and if it is return True else return False
         return sum(res) == root.val            
