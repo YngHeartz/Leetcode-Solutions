32aa04a6738179adb4046e871b8c98452b0bfa3b
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-05-24 21:01:21 -0400

    Time: 53 ms (93.69%) | Memory: 17 MB (61.53%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -1,26 +1,24 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        #Base Case
+        #Base Case for all Trees
         if not root1 and not root2:
             return None
-        #Base case for root1
         if not root1:
             return root2
-        #Base case for root2
         if not root2:
             return root1
         
-        #Creates a variable that will store the added values together at the treenode TreeNode will get the current values in each of the nodes
+        #Creates new Tree with new values added together
         merged = TreeNode(root1.val + root2.val)
-        #recursive call for the right and left subtrees
-        merged.left = self.mergeTrees(root1.left, root2.left)
-        merged.right = self.mergeTrees(root1.right, root2.right)
         
+        #Recursive call for left and right subtrees
+        merged.left = self.mergeTrees(root1.left , root2.left)
+        merged.right = self.mergeTrees(root1.right , root2.right)
+
         return merged
