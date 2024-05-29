6fabdc6f0a8d773eaa0bd0ce5111da51a1e2f847
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-05-29 19:02:33 -0400

    Time: 19 ms (99.85%) | Memory: 16.5 MB (75.99%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,17 +6,13 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        #Empty Array
         res = []
         def helper(root):
-            #Base Case
             if not root:
                 return
-            #Recursive Call on the left subtree and right subtree
+            
             helper(root.left)
             res.append(root.val)
             helper(root.right)
-        #Call the helper function on the root
         helper(root)
-        #Return the res array
-        return res            
+        return res
