d5501d9392ec53753013acc4bc299defd0c80745
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-06-03 18:05:40 -0400

    Time: 42 ms (46.39%) | Memory: 17 MB (31.42%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,19 +9,25 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        #Base case if no node return None
         if not node:
             return None
         
+        #Create an empty dict
         nto = {}
 
         def dfs(node):
+            #If node already in dict return the list of nodes
             if node in nto:
                 return nto[node]
             
+            #Create a copy of the node val and assign it to the nto node
             copy = Node(node.val)
             nto[node] = copy
 
+            #Recursion to create the copy
             for n in node.neighbors:
                 copy.neighbors.append(dfs(n))
             return copy
+            
         return dfs(node)
