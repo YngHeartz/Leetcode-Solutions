4d6b14cb74a704bb9958803ae774eeee483d051a
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-06-10 17:26:30 -0400

    Time: 91 ms (48.11%) | Memory: 18.2 MB (92.72%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,32 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        prevMap = {i: [] for i in range(numCourses)}
+        preMap = {i: [] for i in range(numCourses)}
 
         for crs, pre in prerequisites:
-            prevMap[crs].append(pre)
+            preMap[crs].append(pre)
         
-        Listset = set()
+        listSet = set()
 
         def helper(crs):
-            if crs in Listset:
+            if crs in listSet:
                 return False
-            if prevMap[crs] == []:
+            if preMap[crs] == []:
                 return True
             
-            Listset.add(crs)
+            listSet.add(crs)
 
-            for pre in prevMap[crs]:
+            for pre in preMap[crs]:
                 if not helper(pre):
                     return False
-                    
-            Listset.remove(crs)
-            prevMap[crs] = []
+
+            listSet.remove(crs)
+            preMap[crs] = []
 
             return True
-            
 
         for crs in range(numCourses):
             if not helper(crs):
                 return False
-                
         return True
