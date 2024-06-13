47b4c620975775f7783b034247fbc1d8501e912a
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-06-13 17:30:30 -0400

    Time: 82 ms (90.47%) | Memory: 18.2 MB (92.07%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,41 +1,42 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #Create a map that will store the pre recs inside of the map
+        # Create a map (dictionary) to store the prerequisites for each course
         preMap = {i: [] for i in range(numCourses)}
 
-        #for each cours and prerec inside the prerequisties append the pre to the premap course in the dict
+        # Populate the preMap with the given prerequisites
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
         
-        #Create a empty list that is a set so we can detect infinite loops
-        listset = set()
+        # Initialize a set to keep track of visited courses and detect cycles
+        visited = set()
 
-        #Helper function to itterate through the graph
-        def helper(crs):
-            #Base case if the crs is already in the list we return false and if the preMap is empty return True
-            if crs in listset:
+        # Define a helper function to perform a depth-first search (DFS) on the course graph
+        def dfs(crs):
+            # If the course is already in the visited set, a cycle is detected, return False
+            if crs in visited:
                 return False
-            if preMap[crs] == []:
+            # If the course has no prerequisites, it can be finished, return True
+            if not preMap[crs]:
                 return True
             
-            #Add the crs to the listset
-            listset.add(crs)
+            # Add the course to the visited set to track the current path in the DFS
+            visited.add(crs)
 
-
-            #For each prerec in the preMap run a recursive call on the prerecs in the preMap and if there is not a prerec return False
+            # Recursively perform DFS on all prerequisites of the current course
             for pre in preMap[crs]:
-                if not helper(pre):
+                if not dfs(pre):
                     return False
             
-            #remove the crs from the listset and then set the preMap to empty so we can do the steps over when we call the function recursivly we will start fresh
-            listset.remove(crs)
+            # Remove the course from the visited set and mark it as having no prerequisites
+            visited.remove(crs)
             preMap[crs] = []
 
             return True
 
-        #check to see if the crs is in the numCourses and if it is not return False after recursivly calling to make sure the course is not in there or the prerecs
+        # Check each course to see if it can be completed by performing a DFS
         for crs in range(numCourses):
-            if not helper(crs):
+            if not dfs(crs):
                 return False
 
+        # If all courses can be completed, return True
         return True
