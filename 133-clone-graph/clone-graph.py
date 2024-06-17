43755f5b11058cbc92e5d7a343af86b445efc4b4
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-06-17 17:03:36 -0400

    Time: 52 ms (5.80%) | Memory: 16.7 MB (95.55%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,20 +9,28 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        #Base Case if not node
         if not node:
             return None
         
+        #Creates an empty dictonary
         otn = {}
 
+
+        #Function definition the the paramater of node
         def dfs(node):
+            #If the node is in otn list we will return the node from the otn list
             if node in otn:
                 return otn[node]
             
+            #Create a variable that will store the node value inside of copy and then assign the node in otn to the node value of copy
             copy = Node(node.val)
             otn[node] = copy
 
+            #Loop through the node neighbors left and right to then create the copy of the copy and then return copy
             for n in node.neighbors:
                 copy.neighbors.append(dfs(n))
             return copy
-
+        
+        #Recursive method for all the nodes in the graph
         return dfs(node)
