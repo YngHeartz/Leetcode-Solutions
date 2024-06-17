de01f57904f879b90edab1846325775b68f641e5
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-06-17 16:54:59 -0400

    Time: 100 ms (13.49%) | Memory: 18.2 MB (91.39%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,42 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Create a map (dictionary) to store the prerequisites for each course
         preMap = {i: [] for i in range(numCourses)}
 
-        # Populate the preMap with the given prerequisites
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
-        
-        # Initialize a set to keep track of visited courses and detect cycles
-        visited = set()
 
-        # Define a helper function to perform a depth-first search (DFS) on the course graph
+        listSet = set()
+
         def dfs(crs):
-            # If the course is already in the visited set, a cycle is detected, return False
-            if crs in visited:
+            if crs in listSet:
                 return False
-            # If the course has no prerequisites, it can be finished, return True
-            if not preMap[crs]:
+            if preMap[crs] == []:
                 return True
             
-            # Add the course to the visited set to track the current path in the DFS
-            visited.add(crs)
+            listSet.add(crs)
 
-            # Recursively perform DFS on all prerequisites of the current course
             for pre in preMap[crs]:
                 if not dfs(pre):
                     return False
             
-            # Remove the course from the visited set and mark it as having no prerequisites
-            visited.remove(crs)
+            listSet.remove(crs)
             preMap[crs] = []
 
             return True
-
-        # Check each course to see if it can be completed by performing a DFS
+            
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
-
-        # If all courses can be completed, return True
         return True
