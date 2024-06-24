183b1f4a4f4b308c65a25120cdc05580a40e8718
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-06-24 09:34:02 -0400

    Time: 33 ms (78.79%) | Memory: 16.5 MB (69.08%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,14 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        res = []
-
-        for num in digits:
-            res.append(str(num))
+        number = int(''.join(map(str, digits)))
         
-        comb = ''.join(res)
-
-        comb = int(comb) + 1
-
-        final = list(map(int, str(comb)))
-
-        return final
+        # Add one to the integer
+        number += 1
+        
+        # Convert the integer back to a list of digits and return
+        return list(map(int, str(number)))
