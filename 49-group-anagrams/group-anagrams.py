9456fa37f9aa2dd51a7f7ce561efe91f7c023793
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-08-15 11:47:51 -0400

    Time: 87 ms (70.10%) | Memory: 19.6 MB (83.85%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,19 @@
+# Create a defaultdict(list) to store the sorted key and the word as its values
+# Create a loop that itterates over each item in the strs list
+# Then we want to create a variable that stores the sorted word as a key if its not in the dict already
+# We then append the word to the list
+# Then return a list of sublists
+
+
+
+
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram = defaultdict(list)
+        anagrams = defaultdict(list)
 
         for word in strs:
             sorted_word = ''.join(sorted(word))
-            anagram[sorted_word].append(word)
+            anagrams[sorted_word].append(word)
 
-        return list(anagram.values())
+        return list(anagrams.values())
