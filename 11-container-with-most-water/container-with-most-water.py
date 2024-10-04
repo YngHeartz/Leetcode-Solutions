5c93b0018ffb9e5f0b48c18e1464e48e3f4f65c9
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-10-04 10:13:47 -0400

    Time: 534 ms (31.83%) | Memory: 30 MB (6.94%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,13 +1,23 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        
+        # Initialize left and right pointers and a variable to store the maximum water
         left, right = 0, len(height) - 1
         max_water = 0
+
+        
+        
+        # Iterate while left pointer is less than right pointer
+        # Calculate the width as the distance between left and right
+        # Determine the height by taking the smaller value between height[left] and heigh[right]
+        # Update max_water with the larger of the current max_water or the area calculated
 
         while left < right:
             width = right - left
             heights = min(height[left], height[right])
             max_water = max(max_water, width * heights)
 
+            # Move the left pointer if height[left] is smaller, otherwise move the right pointer
             if height[left] < height[right]:
                 left += 1
             else:
