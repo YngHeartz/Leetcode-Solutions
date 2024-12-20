23e3a987f58ca1b7553e1681efc355b27afca755
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2024-12-20 10:42:53 -0500

    Time: 4 ms (30.96%) | Memory: 19.2 MB (6.01%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,14 +1,11 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # Create empty stack
         res = []
 
-        # Itterate over each token, remove negative sign and append if number is a digit
         for token in tokens:
             if token.lstrip('-').isdigit():
                 res.append(int(token))
             
-            # If not a number, pop the last two digits in the res and preform operand function on numbers
             else:
                 b = res.pop()
                 a = res.pop()
@@ -21,5 +18,4 @@ class Solution:
                 res.append(a * b)
             elif token == '/':
                 res.append(int(a / b))
-        
-        return res[0]
+        return res[-1]
