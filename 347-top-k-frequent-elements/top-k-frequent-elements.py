54ae8d59f535e4b2bc07c99b4326fef3c54a0869
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2025-01-09 11:08:57 -0500

    Time: 4 ms (71.23%) | Memory: 21.2 MB (44.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,8 @@
-# Create a defaultdict(int) to store the count of each number
-# Use a for loop to itterate over each number in nums and update the count to each number
-# Create a variable to store the result in that will be a heap and get all the key values and sort them based on the largest count and then return the k amount of values
-
-
-
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = defaultdict(int)
 
         for num in nums:
             count[num] += 1
-
-        result = heapq.nlargest(k, count.keys(), key=count.get) [:k]
         
-        return result
+        return heapq.nlargest(k, count.keys(), key = count.get)
