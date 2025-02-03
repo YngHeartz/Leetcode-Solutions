1def9a992fd6f7a9ff05e915dc152247058e9d94
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2025-02-03 12:39:32 -0500

    Time: 23 ms (65.56%) | Memory: 23.3 MB (73.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,14 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # create array, initiazlized to the length of nums. filled with 1's
         res = [1] * len(nums)
 
+        # create a postfix that will then be used to take numbers from nums 
+        # multiply the postfix by the num in nums and then replace the res num with the new postfix
         postfix = 1
         for num in range(len(nums)):
             res[num] = postfix
             postfix *= nums[num]
         
+        # do the same thing as the postfix but do it to the numbers before current index
+        
         prefix = 1
-        for num in range(len(nums) -1, -1, -1,):
+        for num in range(len(nums) -1, -1, -1):
             res[num] *= prefix
             prefix *= nums[num]
         
