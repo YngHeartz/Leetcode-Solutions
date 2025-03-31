aef13e03b7164a5f1e57a6346c60c572b6c53fd1
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2025-03-31 10:25:39 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (25.03%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,11 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # create empty set we will add the keys to after each iteration
         seen = set()
 
+        # checking every index in the 9 x 9 board
         for row in range(9):
             for col in range(9):
+                # create a variable that will be holding the value at current index
                 num = board[row][col]
 
+                # if the value at the current index is not a "." meaning there is a number there we will add it to the seen set
+                # if already in seen set we will return "False" because its an invalid sudoku.
+                # if that condition is never met and all the values have been itterated over, we will return "True"
                 if num != '.':
                     row_key = f'{num} in row {row}'
                     col_key = f'{num} in col {col}'
