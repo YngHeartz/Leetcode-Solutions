8f75139fb9cf95aff25edb33557c6d664d4b3737
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2025-04-18 09:44:32 -0400

    Time: 3 ms (95.75%) | Memory: 17.9 MB (80.28%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -2,18 +2,19 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
-        char_map = {}
+        
+        char_dict = {}
         char_set = set()
 
         for char_s, char_t in zip(s, t):
-            if char_s in char_map:
-                if char_map[char_s] != char_t:
+            if char_s in char_dict:
+                if char_dict[char_s] != char_t:
                     return False
+            
             else:
                 if char_t in char_set:
                     return False
-                char_map[char_s] = char_t
+                char_dict[char_s] = char_t
                 char_set.add(char_t)
         
         return True
