8e2fd6755a42721c36dba897fedaa3a8f2fc8636
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2025-04-18 13:27:53 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.31%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -1,13 +1,11 @@
-# start from the end, if current char not a ' ' append to list until ' '.
-# .strip() makes this "   fly me   to   the moon  " to this "fly me   to   the moon"
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         res = []
 
         for char in s.strip() [::-1]:
-                res.append(char)
-                if char.isspace():
-                    return len(res) -1
+            res.append(char)
+            if char.isspace():
+                return len(res) - 1
         
         if ' ' not in s.strip():
             return len(s.strip())
