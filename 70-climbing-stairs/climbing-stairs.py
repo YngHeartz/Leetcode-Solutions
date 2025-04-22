c4693d740fee074eb184b41799f2a1da33a504b9
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2025-04-22 11:57:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,5 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        #  two variables that will be set to one
-        # for each number in range of n - 1
-        # create a variable that will store the last number we were on so that when we shift the 
-        # b variable it will be equal to the last number and we will then add the numbers together and store that into the temp variable until we reach the last value in range of n and then return a since it will the the number of combinations it takes to reach n
-        
         a, b = 1, 1
 
         for _ in range(n - 1):
