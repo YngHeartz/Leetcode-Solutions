fbe9730ba1859984aae6d93d877ae8acfeaead16
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2025-04-28 11:02:56 -0400

    Time: 7 ms (39.97%) | Memory: 17.8 MB (69.76%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -7,15 +7,15 @@ class Solution:
                 num = board[row][col]
 
                 if num != '.':
-                    row_key = f'{num} in row {row}'
-                    col_key = f'{num} in col {col}'
-                    subgrid_key = f'{num} in subgrid {row//3}{col//3}'
+                    rk = f'{num} in row {row}'
+                    ck = f'{num} in col {col}'
+                    sg = f'{num} in subgrid {row//3}{col//3}'
 
-                    if row_key in seen or col_key in seen or subgrid_key in seen:
+                    if rk in seen or ck in seen or sg in seen:
                         return False
                     
-                    seen.add(row_key)
-                    seen.add(col_key)
-                    seen.add(subgrid_key)
+                    seen.add(rk)
+                    seen.add(ck)
+                    seen.add(sg)
         
         return True
