f1d9f484b9026e364b144b6f5303fd9c3245be65
Daniel Hill <108755211+YngHeartz@users.noreply.github.com>
2025-05-01 11:29:05 -0400

    Time: 3 ms (95.59%) | Memory: 17.8 MB (95.50%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -7,11 +7,12 @@ class Solution:
             if char_s in char_dict:
                 if char_dict[char_s] != char_t:
                     return False
-
+            
             else:
                 if char_t in char_set:
                     return False
                 char_dict[char_s] = char_t
                 char_set.add(char_t)
+            
         
         return True
